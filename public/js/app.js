bef896eb219be8aa78beca31dc7ae0366155bd0b
Shingi <shingi2009@gmail.com>
2021-07-07 16:56:18 +0200

    new update

--- public/js/app.js
+++ public/js/app.js
@@ -24784,8 +24784,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.Pusher = __webpack_require__(/*! pusher-js */ "./node_modules/pusher-js/dist/web/pusher.js");
 window.Echo = new laravel_echo__WEBPACK_IMPORTED_MODULE_0__.default({
   broadcaster: 'pusher',
-  key: "915905222fc92149dbb5",
-  wsHost: "127.0.0.1",
+  key: "anything",
+  wsHost: "external.host",
   wsPort: 6001,
   forceTLS: false,
   disableStats: true
